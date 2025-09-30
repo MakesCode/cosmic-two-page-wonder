73c52eaa1f40fd69c800166cdd61acedde05d772
Allan <makescodeur@gmail.com>
2025-09-30 13:50:05 +0200

    update

--- apps/admin/src/router.tsx
+++ apps/admin/src/router.tsx
@@ -2,16 +2,18 @@ import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 export function createRouter() {
-  const router = createTanStackRouter({
+  return createTanStackRouter({
     routeTree,
     scrollRestoration: true,
   })
-  return router
+}
+
+export function getRouter() {
+  return createRouter()
 }
 
 declare module '@tanstack/react-router' {
   interface Register {
-    router: ReturnType<typeof createRouter>
+    router: Awaited<ReturnType<typeof getRouter>>
   }
 }
-
